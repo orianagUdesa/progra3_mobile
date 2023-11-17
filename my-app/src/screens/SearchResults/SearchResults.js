d1310138d28ba40ce8886c2fa72c04496b36f7a4
justinacademartori <mcademartori@udesa.edu.ar>
2023-11-17 15:57:38 -0300

    intentado search, sigue mal

--- my-app/src/screens/SearchResults/SearchResults.js
+++ my-app/src/screens/SearchResults/SearchResults.js
@@ -7,7 +7,7 @@ class SearchResults extends Component {
         super(props)
         this.state={
             usersFiltrados:[],
-            todosUsers: [],
+            users: [],
             textoSearch: ''
         }
     }
@@ -27,13 +27,13 @@ class SearchResults extends Component {
                 })
 
                 this.setState({
-                    todosUsers: dbUsers
+                    users: dbUsers
                 })
             }
         )
     }
 
-    buscarResultados(textoSearch){
+    /*buscarResultados(textoSearch){
         this.state.todosUsers.forEach( user => {
             if (textoSearch.length==0){
                 this.setState({
@@ -44,6 +44,24 @@ class SearchResults extends Component {
                 {null}
                 else{this.state.usersFiltrados.push(user)}
             }
+        })
+    }*/
+
+    buscarResultados(textoSearch){
+        let listaNueva = []
+        for (let i = 0; i < this.state.users.length; i++) {
+            if (this.state.users[i].datos.owner.includes(textoSearch) ) {
+                // IF Para no duplicar usuarios en resultados:
+                if(this.state.usersFiltrados.includes(this.state.users[i]))
+                {null}
+                else{listaNueva.push(this.state.users[i])}
+            }
+            if (textoSearch.length==0){
+                nuevaLista = []
+            }
+        }
+        this.setState({
+            usersFiltrados: listaNueva
         })
     }
 
@@ -56,8 +74,9 @@ class SearchResults extends Component {
                     onChangeText={(text)=> (this.buscarResultados(text), this.setState({textoSearch: text}))}
                     placeholder='Search user'
                     keyboardType='default'
-                    value={this.state.searchText}>
+                    value={this.state.textoSearch}>
                 </TextInput>
+
             </View>
         )
     }
